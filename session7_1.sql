dc47fb5f8afd799f3e85a38d6592b6a52715a9f8
VuDiep1306 <vudiep.1306@gmail.com>
2025-11-22 14:19:43 +0900

    Add files via upload

---
+++ session7_1.sql
@@ -0,0 +1,39 @@
+create database SaleDB;
+use SaleDB;
+create table Customers (
+CustomerID int primary key auto_increment,
+CustomerName varchar(100) not null,
+Phone varchar(15),
+CreatedAt datetime
+);
+create table Products (
+ProductID int primary key auto_increment,
+ProductName varchar(255) not null,
+Category varchar(255),
+Price decimal(10,2) not null
+);
+create table Orders (
+OrderID int primary key auto_increment,
+CustomerID int,
+TotalAmount decimal(10,2) not null,
+FOREIGN KEY (CustomerID) REFERENCES Customers (CustomerID)
+);
+create table OrderDetails (
+OrderDetailID int primary key auto_increment,
+OrderID int,
+ProductID int,
+Quantity int not null,
+UnitPrice decimal(10,2) not null,
+foreign key (OrderID) references Orders (OrderID),
+foreign key (productID) references Products (ProductID)
+);
+
+-- Thêm chỉ số cho cột Email trong bảng Customers
+ALTER TABLE Customers
+ADD COLUMN Email VARCHAR(255) NOT NULL;
+CREATE INDEX idx_email ON Customers(Email);
+
+--  Thêm chỉ số cho cột OrderDate trong bảng Orders.
+ALTER TABLE Orders
+ADD COLUMN OrderDate DATETIME NOT NULL,
+ADD INDEX idx_orderDate (OrderDate);
